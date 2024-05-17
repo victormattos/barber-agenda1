208ecba67ced1d71f306e50ac3bbb3ddafed30f4
victormattos <83734508+victormattos@users.noreply.github.com>
2024-05-17 08:18:32 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config(); // Carregar variáveis de ambiente do arquivo .env
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -17,13 +17,19 @@ const db = mysql.createPool({
 app.use(express.json());
 app.use(cors());
 
-// Rotas
-app.post("/register", (req, res) => {
+// Rota de teste para verificar a conexão com a API
+app.get('/api/test', (req, res) => {
+    res.send('API funcionando!');
+});
+
+// Rota para registrar um novo cliente
+app.post('/register', (req, res) => {
     const { nome, email, fone, data, hora } = req.body;
     let sql = "INSERT INTO cliente (nome, email, fone, data, hora) VALUES (?,?,?,?,?)";
     db.query(sql, [nome, email, fone, data, hora], (err, result) => {
         if (err) {
             console.log(err);
+            res.status(500).send(err);
         } else {
             console.log(result);
             res.send(result);
@@ -31,18 +37,21 @@ app.post("/register", (req, res) => {
     });
 });
 
-app.get("/cliente", (req, res) => {
+// Rota para obter todos os clientes
+app.get('/cliente', (req, res) => {
     let sql = "SELECT * FROM cliente";
     db.query(sql, (err, result) => {
         if (err) {
             console.log(err);
+            res.status(500).send(err);
         } else {
             res.send(result);
         }
     });
 });
 
-app.put("/edit", (req, res) => {
+// Rota para editar um cliente
+app.put('/edit', (req, res) => {
     const { id, nome, email, fone, data, hora } = req.body;
     console.log("Server received edit request with the following data:", req.body);
     let sql = "UPDATE cliente SET nome = ?, email = ?, fone = ?, data = ?, hora = ? WHERE id = ?";
@@ -57,11 +66,17 @@ app.put("/edit", (req, res) => {
     });
 });
 
-app.delete("/delete/:index", (req, res) => {
+// Rota para deletar um cliente
+app.delete('/delete/:index', (req, res) => {
     const { index } = req.params;
     let sql = "DELETE FROM cliente WHERE id = ?";
     db.query(sql, [index], (err, result) => {
-        err ? console.log(err) : res.send(result);
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.send(result);
+        }
     });
 });
 
