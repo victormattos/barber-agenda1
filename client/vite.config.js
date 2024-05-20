6df619935f139012bdb78b60190c0c2192ab8ee1
victormattos <83734508+victormattos@users.noreply.github.com>
2024-05-20 18:39:27 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://igor-dias-barber-agendamentos-production.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
