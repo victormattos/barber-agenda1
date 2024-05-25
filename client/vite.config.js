f9924b6b02a75c7f0dc92bb0b61faf85e19e8f58
victormattos <83734508+victormattos@users.noreply.github.com>
2024-05-25 14:56:17 -0300

    Novas correções para dos arquivos dialog.jsx, vercel.json, e App.jsx.

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://igor-dias-barber-agendamentos-production.up.railway.app',
+        target: 'https://igor-dias-barber-agendamentos-production.up.railway.app/api',
         changeOrigin: true,
         secure: false,
       },
